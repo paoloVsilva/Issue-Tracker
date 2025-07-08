60377183716be2a8ce30caa2a285bbedc275dd9d
paolosilva <silva.paolo.10@gmail.com>
2025-07-08 17:03:42 +0800

    refactor: Move inline function

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,17 +1,17 @@
 'use client'
 
-import { Button, Callout, Text, TextField } from '@radix-ui/themes'
-import { useForm, Controller } from 'react-hook-form'
-import 'easymde/dist/easymde.min.css'
-import dynamic from 'next/dynamic'
-import axios from 'axios'
-import { useRouter } from 'next/navigation'
-import { useState } from 'react'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { createIssueSchema } from '@/app/validationSchemas'
-import { z } from 'zod'
 import ErrorMessage from '@/app/components/ErrorMessage'
 import Spinner from '@/app/components/Spinner'
+import { createIssueSchema } from '@/app/validationSchemas'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Button, Callout, TextField } from '@radix-ui/themes'
+import axios from 'axios'
+import 'easymde/dist/easymde.min.css'
+import dynamic from 'next/dynamic'
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
+import { Controller, useForm } from 'react-hook-form'
+import { z } from 'zod'
 
 const SimpleMdeEditor = dynamic(() => import('react-simplemde-editor'), {
   ssr: false
@@ -32,6 +32,17 @@ const NewIssuePage = () => {
   const [error, setError] = useState('')
   const [isSubmitting, setSubmitting] = useState(false)
 
+  const onSubmit = handleSubmit(async data => {
+    try {
+      setSubmitting(true)
+      await axios.post('/api/issues', data)
+      router.push('/issues')
+    } catch (error) {
+      setError('An unexpected error occurred.')
+      setSubmitting(false)
+    }
+  })
+
   return (
     <div className='max-w-xl'>
       {error && (
@@ -39,18 +50,7 @@ const NewIssuePage = () => {
           <Callout.Text>{error}</Callout.Text>
         </Callout.Root>
       )}
-      <form
-        onSubmit={handleSubmit(async data => {
-          try {
-            setSubmitting(true)
-            await axios.post('/api/issues', data)
-            router.push('/issues')
-          } catch (error) {
-            setError('An unexpected error occurred.')
-            setSubmitting(false)
-          }
-        })}
-      >
+      <form onSubmit={onSubmit}>
         <TextField.Root
           placeholder='Title'
           {...register('title')}

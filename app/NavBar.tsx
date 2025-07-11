f64dceeff5fcfec6415671ef246874a8548fb4ee
paolosilva <silva.paolo.10@gmail.com>
2025-07-11 10:20:20 +0800

    Add login and logout links

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -3,10 +3,14 @@
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import { AiFillBug } from 'react-icons/ai'
+import { useSession } from 'next-auth/react'
 import classNames from 'classnames'
+import { Box } from '@radix-ui/themes'
 
 const NavBar = () => {
   const currentPath = usePathname()
+  const { status, data: session } = useSession()
+
   const links = [
     { label: 'Dashboard', href: '/' },
     { label: 'Issues', href: '/issues' }
@@ -19,19 +23,29 @@ const NavBar = () => {
       </Link>
       <ul className='flex space-x-6'>
         {links.map(link => (
-          <Link
-            key={link.href}
-            href={link.href}
-            className={classNames({
-              'text-zinc-950': link.href === currentPath,
-              'text-zinc-500': link.href !== currentPath,
-              'hover:text-zinc-800 transition-colors': true
-            })}
-          >
-            {link.label}
-          </Link>
+          <li key={link.href}>
+            <Link
+              href={link.href}
+              className={classNames({
+                'text-zinc-950': link.href === currentPath,
+                'text-zinc-500': link.href !== currentPath,
+                'hover:text-zinc-800 transition-colors': true
+              })}
+            >
+              {link.label}
+            </Link>
+          </li>
         ))}
       </ul>
+      <Box>
+        {status === 'authenticated' && (
+          <Link href='/api/auth/signout'>Log out</Link>
+        )}
+
+        {status === 'unauthenticated' && (
+          <Link href='/api/auth/signin'>Login</Link>
+        )}
+      </Box>
     </nav>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import NavBar from './NavBar'
+import AuthProvider from './auth/Provider'
 
 const inter = Inter({
   subsets: ['latin'],
@@ -24,12 +25,14 @@ export default function RootLayout({
   return (
     <html lang='en' className={inter.variable}>
       <body>
-        <Theme accentColor='violet'>
-          <NavBar />
-          <main className='p-5'>
-            <Container>{children}</Container>
-          </main>
-        </Theme>
+        <AuthProvider>
+          <Theme accentColor='violet'>
+            <NavBar />
+            <main className='p-5'>
+              <Container>{children}</Container>
+            </main>
+          </Theme>
+        </AuthProvider>
       </body>
     </html>
   )

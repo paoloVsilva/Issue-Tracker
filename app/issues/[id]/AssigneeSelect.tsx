a0868d366e827f3cd16574c31d623c244153a890
paolosilva <silva.paolo.10@gmail.com>
2025-07-11 15:16:58 +0800

    Populate assignee component

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,13 +1,32 @@
+'use client'
+
+import { User } from '@/app/generated/prisma'
 import { Select } from '@radix-ui/themes'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 const AssigneeSelect = () => {
+  const [users, setUsers] = useState<User[]>([])
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const { data } = await axios.get<User[]>('/api/users')
+      setUsers(data)
+    }
+    fetchUser()
+  }, [])
+
   return (
     <Select.Root>
-      <Select.Trigger placeholder='Assign...' />
-      <Select.Content>
+      <Select.Trigger placeholder='Assignee' />
+      <Select.Content variant='soft' position='popper'>
         <Select.Group>
           <Select.Label>Suggestions</Select.Label>
-          <Select.Item value='1'>Paolo Silva</Select.Item>
+          {users.map(user => (
+            <Select.Item key={user.id} value={user.id.toString()}>
+              {user.name}
+            </Select.Item>
+          ))}
         </Select.Group>
       </Select.Content>
     </Select.Root>
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,5 +1,5 @@
 import { prisma } from '@/prisma/client'
-import { Box, Flex, Grid } from '@radix-ui/themes'
+import { Box, Flex, Grid, Separator } from '@radix-ui/themes'
 import { notFound } from 'next/navigation'
 import EditIssueButton from './EditIssueButton'
 import IssueDetails from './IssueDetails'
@@ -30,6 +30,7 @@ const IssueDetailPage = async ({ params }: Props) => {
         {session && (
           <Flex direction='column' gap='4'>
             <AssigneeSelect />
+            <Separator orientation='horizontal' size='4' />
             <ChangeStatusButton status={issue.status} issueId={issue.id} />
             {issue.status === 'OPEN' && <EditIssueButton issueId={issue.id} />}
             <DeleteIssueButton issueId={issue.id} />
